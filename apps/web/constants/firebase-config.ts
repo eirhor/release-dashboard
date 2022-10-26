15a238b80e8fad448127cd200a5e6527bd587a93
Eirik Horvath <horvath.eirik@gmail.com>
2022-10-26 12:54:06 +0200

    setup tailwind, firebase

---
+++ apps/web/constants/firebase-config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDrb9TSrmqOnJyHB_whFB7Jn4tAME5NdSI",
+  authDomain: "release-dashboard-e8a8e.firebaseapp.com",
+  projectId: "release-dashboard-e8a8e",
+  storageBucket: "release-dashboard-e8a8e.appspot.com",
+  messagingSenderId: "1021753361797",
+  appId: "1:1021753361797:web:c87dd0fb0e918657aeb0ec",
+  measurementId: "G-DTC16HV639",
+};
