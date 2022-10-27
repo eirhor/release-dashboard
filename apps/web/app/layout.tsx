99c28f88a9682baa2d575827bdaf1e2ea0cbe501
Ole Skaar <ole.skaar@geta.no>
2022-10-27 16:08:56 +0200

    Add Checkbox component, improve update URL util

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,6 +1,7 @@
 import "../styles/globals.css";
 import { Roboto } from "@next/font/google";
 import { Search } from "./Search";
+import { CheckBox } from "./Checkbox";
 
 const robotoNormal = Roboto({ weight: "400" });
 
@@ -16,8 +17,9 @@ export default function RootLayout({
         <header className="col-span-5 row-span-1 flex items-center border-b-2 border-solid">
           <h1 className="p-2 text-xl">🏷️ GCC Release Dashboard</h1>
         </header>
-        <aside className="col-span-1 row-span-2 border-r-2 border-solid p-4">
+        <aside className="col-span-1 row-span-2 space-y-4 border-r-2 border-solid p-4">
           <Search />
+          <CheckBox name="alpha" label="Include alpha releases?" />
         </aside>
         <main className="col-span-4 col-start-2 row-span-2 p-2">
           {children}
--- apps/web/utils/update-url-params.ts
+++
@@ -1,6 +0,0 @@
-export const updateUrlParameter = (key: string, value: string) => {
-    const newURL = new URL(window.location.href);
-    newURL.searchParams.set(key, value);
-    newURL.searchParams.sort();
-    history.pushState(undefined, '', newURL.href);
-};
