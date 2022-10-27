b69e6c970b440c7f2f538fe61907ba1cfb6b2ce1
Eirik Horvath <horvath.eirik@gmail.com>
2022-10-27 10:21:49 +0200

    feat: tailwind setup, data fetching for details

--- apps/web/app/[id]/page.tsx
+++ apps/web/app/[id]/page.tsx
@@ -1,3 +1,24 @@
-export default function Page() {
-    return <h1>Release details page</h1>
+import type { ParsedUrlQuery } from "querystring";
+
+export const fetchReleaseDetails = (id: string) => {
+  return fetch(
+    `https://api.github.com/repos/Geta/component-libraries/releases/${id}`,
+    {
+      headers: {
+        Accept: "application/vnd.github+json",
+        Authorization: "Bearer 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",
+      },
+    }
+  ).then((r) => r.json());
+};
+
+export interface IPageProps {
+  params: ParsedUrlQuery;
+  searchParams: ParsedUrlQuery;
+}
+
+export default async function Page({ params }: IPageProps) {
+  const data = await fetchReleaseDetails(params["id"] as string);
+
+  return <h1>Release details page</h1>;
 }
