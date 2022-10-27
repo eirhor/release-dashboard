06c60a513ddc2db6e828808e6493b1a68e46d34e
Eirik Horvath <horvath.eirik@gmail.com>
2022-10-27 11:11:48 +0200

    feat: populate content for details page, add darkmode support

--- apps/web/app/[id]/page.tsx
+++ apps/web/app/[id]/page.tsx
@@ -1,4 +1,8 @@
 import type { ParsedUrlQuery } from "querystring";
+import Image from "next/image";
+import { BsTag } from "react-icons/bs";
+import { FiGitCommit } from "react-icons/fi";
+import ReactMarkdown from "react-markdown";
 
 export const fetchReleaseDetails = (id: string) => {
   return fetch(
@@ -6,10 +10,16 @@ export const fetchReleaseDetails = (id: string) => {
     {
       headers: {
         Accept: "application/vnd.github+json",
-        Authorization: "Bearer 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",
+        Authorization: `Bearer ${process.env.GH_ACCESS_TOKEN}`,
       },
     }
-  ).then((r) => r.json());
+  ).then((r) => {
+    if (r.status !== 200) {
+      throw Error(r.statusText);
+    }
+
+    return r.json();
+  });
 };
 
 export interface IPageProps {
@@ -20,5 +30,34 @@ export interface IPageProps {
 export default async function Page({ params }: IPageProps) {
   const data = await fetchReleaseDetails(params["id"] as string);
 
-    return <section>Release details here</section>;
+  return (
+    <section>
+      <h1>{data.tag_name}</h1>
+      <div>
+        <span>
+          <Image
+            src={data.author.avatar_url}
+            alt={data.author.login}
+            width={20}
+            height={20}
+          />
+          <p>
+            <strong>{data.author.login}</strong> released this{" "}
+            {data.published_at}
+          </p>
+        </span>
+        <p>
+          <BsTag />
+          <span>{data.tag_name}</span>
+        </p>
+        <p>
+          <FiGitCommit />
+          <span>{data.target_commitish}</span>
+        </p>
+      </div>
+      <div>
+        <ReactMarkdown>{data.body}</ReactMarkdown>
+      </div>
+    </section>
+  );
 }
--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -8,11 +8,13 @@ export default function RootLayout({
   return (
     <html>
       <head></head>
-      <body className="min-h-screen grid grid-rows-[5rem_auto] grid-cols-5">
+      <body className="min-h-screen grid grid-rows-[5rem_auto] grid-cols-5 bg-white dark:bg-slate-800 text-slate-800 dark:text-white">
         <header className="border-solid border-b-2 row-span-1 col-span-5 flex items-center">
           <h1 className="text-xl p-2">🏷️ GCC Release Dashboard</h1>
         </header>
-        <aside className="row-span-2 col-span-1 p-2 border-solid border-r-2">Sidebar</aside>
+        <aside className="row-span-2 col-span-1 p-2 border-solid border-r-2">
+          Sidebar
+        </aside>
         <main className="row-span-2 col-start-2 col-span-4 p-2">
           {children}
         </main>
